86c073ec264389d85c26d9cc43892589b7d60dda
Hovig Kousherian <hovig.kousherian@gmail.com>
2018-01-23 19:05:38 -0500

    Updated AppConstants.

--- Don't Get Cold/Application/AppConstants.swift
+++ Don't Get Cold/Application/AppConstants.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+struct AppConstants {
+    
+    static let WeatherApiKey = "bbcf61f77b09ced0996ff61969903587"
+    
+    enum Encoding: String {
+        case weather
+        case coord
+        case lat
+        case lon
+        case base
+        case visibility
+        case wind
+        case clouds
+        case dt
+        case id
+        case name
+        case cod
+        case main
+        case description
+        case icon
+        case temp
+        case pressure
+        case humidity
+        case temp_min
+        case temp_max
+        case speed
+        case deg
+    }
+}
+
+
