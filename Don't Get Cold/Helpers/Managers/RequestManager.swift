18d5a1c0efe2246c8b328ca4c4dd9c5fa5300a9c
Hovig Kousherian <hovig.kousherian@gmail.com>
2018-01-23 22:03:35 -0500

    Created RequestManager.

---
+++ Don't Get Cold/Helpers/Managers/RequestManager.swift
@@ -0,0 +1,29 @@
+//
+//  RequestManager.swift
+//  Don't Get Cold
+//
+//  Created by Hovig Kousherian on 1/23/18.
+//  Copyright © 2018 Hovig Kousherian. All rights reserved.
+//
+
+import Foundation
+
+class RequestManager {
+    
+    //MARK: Members
+    static let session = URLSession(configuration: .default)
+    static var dataTask: URLSessionDataTask?
+    
+    static func request(withURL url: String, parameters: String, compeletion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        dataTask?.cancel()
+        if var urlComponents = URLComponents(string: url) {
+            urlComponents.query = parameters
+            guard let url = urlComponents.url else { return }
+            
+            session.dataTask(with: url, completionHandler: { (data, responce, error) in
+                compeletion(data, responce, error)
+            })
+        }
+        dataTask?.resume()
+    }
+}
