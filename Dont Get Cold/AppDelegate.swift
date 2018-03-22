07b70a9255660204fb1706cc5c749edd4ed39c83
Hovig Kousherian <hovig.kousherian@gmail.com>
2018-03-21 22:53:16 -0400

    Updated UserDefaults extension and CacheManager inorder to transfer the old data saved in standard UserDefaults to the shared app group UserDefault.

--- Dont Get Cold/AppDelegate.swift
+++ Dont Get Cold/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        CacheManager.cache.convertPastDataToGourp()
+        
         application.shortcutItems = ShortcutManager.shared.shortcutItems()
         
         return true
--- Dont Get Cold/Helpers/Extensions/Foundation/UserDefaults+Ext.swift
+++ Dont Get Cold/Helpers/Extensions/Foundation/UserDefaults+Ext.swift
@@ -9,11 +9,10 @@
 import Foundation
 
 extension UserDefaults {
-
     
     static func save(object: AnyObject, forKey key: AppConstants.CachingKey) {
         let data = NSKeyedArchiver.archivedData(withRootObject: object)
-        if let userDefaults = UserDefaults(suiteName: "group.com.hovigkousherian.Dont-Get-Cold") {
+        if let userDefaults = UserDefaults(suiteName: AppConstants.groupBundleIndentifier) {
             userDefaults.set(data, forKey: key.rawValue)
             userDefaults.synchronize()
         } else {
@@ -24,7 +23,7 @@ extension UserDefaults {
     
     static func get(objectForKey key: AppConstants.CachingKey) -> AnyObject? {
         let data: Data?
-        if let userDefaults = UserDefaults(suiteName: "group.com.hovigkousherian.Dont-Get-Cold") {
+        if let userDefaults = UserDefaults(suiteName: AppConstants.groupBundleIndentifier) {
             data = userDefaults.object(forKey: key.rawValue) as? Data
         } else {
             data = standard.object(forKey: key.rawValue) as? Data
@@ -37,10 +36,21 @@ extension UserDefaults {
     }
     
     static func remove(objectForKey key: AppConstants.CachingKey) {
-        if let userDefaults = UserDefaults(suiteName: "com.hovigkousherian.Dont-Get-Cold") {
+        if let userDefaults = UserDefaults(suiteName: AppConstants.groupBundleIndentifier) {
             userDefaults.removeObject(forKey: key.rawValue)
         } else {
             standard.removeObject(forKey: key.rawValue)
         }
+    }
+    
+    static func convertPastDataToGourp(forKey key: String) {
+        guard let userDefaults = UserDefaults(suiteName: AppConstants.groupBundleIndentifier),
+        let standardUserDefaultData = standard.object(forKey: key) as? Data else {
+            return
+        }
+        
+        userDefaults.set(standardUserDefaultData, forKey: key)
+        userDefaults.synchronize()
+        standard.removeObject(forKey: key)
     }
 }
